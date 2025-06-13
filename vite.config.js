bf42605e5a2c5f8e195278f1003f459963d5102b
SamNanton <trikkiwun@gmail.com>
2025-06-13 06:07:00 -0400

    still trying to deploy to /metaAnalysis

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/metaAnalysis/'
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
